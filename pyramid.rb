c45b32b12e607d0f3c36ec531ee0e307f510b674
Anabel Arbeeny <anabela0701@hstat.org>
2018-10-05 16:13:02 +0000

    part 2

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,27 @@
 #write your code here
+#$mario = " 
+           #  #
+          ##  ##
+         ###  ###
+        ####  ####
+       #####  #####"
+#puts"what height would you like the pyramid?"
+#user_height = gets.chomp.to_i
+
+
+def pyramid()
+    puts"what height would you like the pyramid?"
+    height = gets.chomp.to_i
+    height.to_i
+    i= height
+    loop do
+        puts " "*i + "#"*(height-i) + " "*2 + "#"*(height-i)
+        i -= 1
+        if i == -1
+            break 
+        end 
+    end 
+
+end
+
+pyramid()
